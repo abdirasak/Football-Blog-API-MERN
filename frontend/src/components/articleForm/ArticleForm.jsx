463db5a5150135ff4d7923f2b8e424c78fcf9bb5
abdirasak <abdi_2020@hotmail.co.uk>
2022-10-25 21:46:09 +0100

    worked on article form

--- frontend/src/components/articleForm/ArticleForm.jsx
+++ frontend/src/components/articleForm/ArticleForm.jsx
@@ -5,16 +5,20 @@ import './ArticleForm.css'
 function ArticleForm() {
 
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+    league: '',
+    team: '',
+    title: '',
+    article: '',
+    articleImage: ''
   })
 
-  const { email, password } = formData
+  const { league, team, title, article, articleImage } = formData
 
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
+
 
     }))
   }
@@ -24,6 +28,7 @@ function ArticleForm() {
       <section className='f-heading'>
         <h3 style={{ marginTop: '15px' }}>
           <FaFileAlt /> Create an article
+
         </h3>
       </section>
 
@@ -33,50 +38,50 @@ function ArticleForm() {
             <input
               type="text"
               className="f-control"
-              id='email'
-              name='email'
-              value={email} onChange={onChange}
-              placeholder='Enter your email' />
+              id='league'
+              name='league'
+              value={league} onChange={onChange}
+              placeholder='Enter league name' />
           </div>
 
           <div className="f-group">
             <input
-              type="password"
+              type="text"
               className="f-control"
-              id='password'
-              name='password'
-              value={password} onChange={onChange}
+              id='team'
+              name='team'
+              value={team} onChange={onChange}
+              placeholder='Enter team name' />
+          </div>
+
+          <div className="f-group">
+            <input
+              type="text"
+              className="f-control"
+              id='title'
+              name='title'
+              value={title} onChange={onChange}
               placeholder='Enter password' />
           </div>
 
           <div className="f-group">
             <input
-              type="password"
+              type="textarea"
               className="f-control"
-              id='password'
-              name='password'
-              value={password} onChange={onChange}
-              placeholder='Enter password' />
+              id='article'
+              name='article'
+              value={article} onChange={onChange}
+              placeholder='Enter article' />
           </div>
 
           <div className="f-group">
             <input
-              type="password"
+              type="file"
               className="f-control"
-              id='password'
-              name='password'
-              value={password} onChange={onChange}
-              placeholder='Enter password' />
-          </div>
-
-          <div className="f-group">
-            <input
-              type="password"
-              className="f-control"
-              id='password'
-              name='password'
-              value={password} onChange={onChange}
-              placeholder='Enter password' />
+              id='articleImage '
+              name='articleImage '
+              value={articleImage} onChange={onChange}
+              placeholder='Enter image' />
           </div>
 
           <div className="f-group">
